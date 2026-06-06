4705e13927bf1d563f2030f8c61f27c646ec7f7f
arya-5990 <aryasha4906c@gmail.com>
2026-06-06 11:32:53 +0530

    feat: restore website access by removing BlockedBanner component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,19 +6,9 @@ import GallerySection from '../components/Gallery';
 import TestimonialsSection from '../components/Testimonials';
 import CallToAction from '../components/CallToAction';
 
-// TODO: Remove or comment out this import once the outstanding Firebase bill is paid.
-import BlockedBanner from '../components/BlockedBanner';
-
 export default function Home() {
   return (
     <main>
-      {/* 
-        BLOCK SCREEN: This component covers the entire landing page.
-        To restore the website and remove the block screen after the bill is paid, 
-        simply comment out or delete the line below:
-      */}
-      <BlockedBanner />
-      
       <Hero />
       <HomePricing />
       <MembershipCardPromo />
@@ -29,4 +19,5 @@ export default function Home() {
     </main>
   );
 }
+
 
